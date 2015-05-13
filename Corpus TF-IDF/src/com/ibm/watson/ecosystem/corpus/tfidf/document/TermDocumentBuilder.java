05f81076566144ae41ffbf363045a9ef04a27bfa
Will Beason <willbeason@gmail.com>
2015-05-13 15:48:16 -0500

    Refreshing files ...

--- Corpus TF-IDF/src/com/ibm/watson/ecosystem/corpus/tfidf/document/TermDocumentBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.watson.ecosystem.corpus.tfidf.document;
-
-import com.ibm.watson.ecosystem.corpus.document.DocumentBuilder;
-
-public final class TermDocumentBuilder extends DocumentBuilder {
-  
-  
-  
-}
